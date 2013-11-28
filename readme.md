8056af631d44401b9f8d3c037dc969385ae6adda
ShaiMesisterano <shai@mcterano.com>
2013-11-28 15:09:07 +0200

    Removed unused files. Changed configuration parameters

--- readme.md
+++ readme.md
@@ -1,15 +1,14 @@
 <h1>BlindJS</h1>
 <h2>Don't look at what you're typing, BlindJS will display your pre-defined code.</h2>
-BlindJS is a Javascript library that replaces textarea content with pre-defined code in your web pages.
+BlindJS is a Javascript library that replaces <u>content</u> with <u>pre-defined code</u> in your text editor or web page.
 
 <h3>Quick install</h3>
-
 Include the Javascript code before <pre>&lt;/body&gt;</pre>:
 
 Create a wrapper and insert your code:
 
 <pre>
-&lt;div id="myDiv"&gt;
+&lt;div id="divElement"&gt;
     function func(){
         alert("Super Awesome!");
     }
@@ -19,16 +18,27 @@ Create a wrapper and insert your code:
 Add textarea placeholder:
 
 <pre>
-&lt;textarea id="myTextarea"&gt;
+&lt;textarea id="textareaElement"&gt;
 &lt;/textarea&gt;
 </pre>
 
-Start BlindJS:
+Start BlindJS with reference to:
+<ol>
+<li>input text</li>
+<li>output element</li>
+</ol>
 
 <pre>
-BlindJS.start("myDiv", "myTextarea");
+BlindJS.start({
+input: divElement.innerText,
+output: textareaElement
+});
 </pre>
 
-<h3>About</h3>
 
+<h3>IDE Support</h3>
+New! BlindJS is enabled in <a href="http://download.brackets.io" target="_blank">Adobe Brackets</a>.
+Check out the extension: <a href="https://github.com/ShaiMesisterano/brackets-blindjs">https://github.com/ShaiMesisterano/brackets-blindjs</a>
+
+<h3>About</h3>
 BlindJS is created by <a href="http://www.mcterano.com/blog" title="Shai Mesisterano">Shai Mesisterano</a> and is licensed under <a href="https://raw.github.com/ShaiMesisterano/BlindJS/master/LICENSE" title="MIT LICENSE">MIT</a>.
--- test/spec.js
+++ test/spec.js
@@ -1,49 +1,87 @@
 describe("BlindJS Library", function () {
-	describe("Given an input div and an output textarea", function () {
+	beforeEach(function () {
+		var cfg = {};
+	});
+	describe("Given being in 'browser' mode", function () {
 		beforeEach(function () {
-			// Create dummy DIV
+			// Create dummy elements
 			var divElement = document.createElement("div");
-			divElement.id = "input";
-			divElement.innerText = "abc";
-			document.body.appendChild( divElement );
-			// Create dummy TextBox
-			textareaElement  = document.createElement("textarea");
-			textareaElement.id = "output";
-			document.body.appendChild( textareaElement);
+			var textareaElement  = document.createElement("textarea");
 
-			BlindJS.start("input", "output");
+			divElement.id = "input";
+			divElement.innerHTML = "abc";
+			textareaElement.id = "output";
+			document.body.appendChild( divElement );
+			document.body.appendChild( textareaElement );
+			cfg = {
+				input: divElement.innerHTML,
+				output: textareaElement
+			};
+			BlindJS.start(cfg);
 		});
-		describe("When starting", function () {
+		describe("When configured", function(){
 			it("Should save output element as 'BlindJS.$el'", function () {
 				expect(BlindJS.$el.id).toBe("output");
 			});
-			it("Should save content as 'BlindJS.content'", function () {
-				expect(BlindJS.content).toBe("abc");
-			});
 		});
-		describe("When typing a random caharachter", function () {
-			beforeEach(function () {
-				BlindJS.typed = "";
-				BlindJS.type("a");
-			});
-			it("Should print charachter from the stored content", function () {
-				expect(BlindJS.typed).toBe("a");
-			});
+	});
+	describe("Given being in 'brackets' mode", function () {
+		var EditorManager = {
+			getFocusedEditor: function(){
+				this.getSelectedText = function(){
+					return "abc";
+				};	
+				this._codeMirror = {
+					replaceSelection: function(text){
+						return text;
+					},
+					setValue: function(text){
+						return text;
+					}
+				};
+			}
+		};
+		beforeEach(function(){
+			var selection = "abc";
+			var text = "123";
+			cfg = {
+				input: selection,
+				output: text
+			};
+			BlindJS.start(cfg);
 		});
-		describe("When typing random characters", function () {
-			beforeEach(function () {
-				BlindJS.column = 0;
-				BlindJS.typed = "";
-				BlindJS.type("1");
-				BlindJS.type("2");
-				BlindJS.type("3");
-			});
-			it("Should count the number of key strokes", function () {
-				expect(BlindJS.column).toBe(3);
-			});
-			it("Should print characters from the stored content", function () {
-				expect(BlindJS.typed).toBe("abc");
-			});
+	});
+	describe("When starting", function () {
+		it("Should save content as 'BlindJS.content'", function () {
+			expect(BlindJS.content).toBe("abc");
+		});
+	});
+	describe("When typing a random character", function () {
+		beforeEach(function () {
+			BlindJS.typed = "";
+			BlindJS.type("a");
+		});
+		it("Should print charachter from the stored content", function () {
+			expect(BlindJS.typed).toBe("a");
+		});
+	});
+	describe("When typing random characters", function () {
+		beforeEach(function () {
+			BlindJS.column = 0;
+			BlindJS.typed = "";
+			BlindJS.type("1");
+			BlindJS.type("2");
+			BlindJS.type("3");
+		});
+		it("Should count the number of key strokes", function () {
+			expect(BlindJS.column).toBe(3);
+		});
+		it("Should print characters from the stored content", function () {
+			expect(BlindJS.typed).toBe("abc");
+		});
+		it("Should limit the length of typed content", function() {
+			BlindJS.type("4");
+			expect(BlindJS.typed).toBe("abc");
 		});
 	});
 });
