df2e61b44090bfaa62862fe16ad4c089445f71dd
shaymesistrano <SMesisterano@mycwt.com>
2020-01-25 11:49:03 +0200

    add vscode and brackets extensions

---
+++ extensions/vscode/README.md
@@ -0,0 +1,3 @@
+<h1>BlindJS</h1>
+BlindJS is an extremely useful tool for <u>lectures, lessons and presentations</u>.<br />
+It allows you to write code samples quickly and without any chance for mistakes.
---
+++ extensions/vscode/extension.js
@@ -0,0 +1,90 @@
+const vscode = require('vscode');
+var BlindJS = (function (blindJS) {
+	blindJS.content = {};
+
+	blindJS.saveState = function () {
+		this.typed += this.content.slice(this.column, this.column + 1);
+		this.column += 1;
+	};
+
+	blindJS.type = function () {
+		blindJS.saveState();
+		return this.typed;
+	};
+
+	blindJS.start = function ({ input }) {
+		this.column = 0;
+		this.typed = "";
+		this.content = input;
+	};
+
+	return blindJS;
+
+})(BlindJS || {});
+
+const getCompleteDocumentRange = document => {
+	const firstLine = document.lineAt(0);
+	const lastLine = document.lineAt(document.lineCount - 1);
+	return new vscode.Range(firstLine.range.start, lastLine.range.end);
+};
+
+let disposableType;
+function activate(context) {
+	console.log('activate');
+	
+	const disposableOpenWithBlindJs = vscode.commands.registerCommand('extension.openWithBlindJs', function ({ fsPath }) {
+		const setting = vscode.Uri.parse(fsPath);
+
+		disposableType = vscode.commands.registerCommand('type', () => {
+			const typed = BlindJS.type();
+			const { _line, _character } = vscode.window.activeTextEditor.selection.anchor;
+		
+			vscode.window.activeTextEditor.edit(activeEditor => {
+				let newPosition;
+				let newSelection;
+		
+				activeEditor.replace(getCompleteDocumentRange(activeEditor._document), typed);
+		
+				newPosition = new vscode.Position(_line + 1, _character + 1);
+				newSelection = new vscode.Selection(newPosition, newPosition);
+		
+				vscode.window.activeTextEditor.selection = newSelection;
+			});
+		});
+
+		vscode.workspace.openTextDocument(setting).then((a) => {
+			vscode.window.showTextDocument(a).then(e => {
+				e.edit(edit => {
+					const { _document } = edit;
+					const input = _document.getText();
+
+					BlindJS.start({
+						input
+					});
+
+					edit.delete(getCompleteDocumentRange(_document));
+				});
+			});
+		}, (error) => {
+			console.error({ error });
+		});
+	});
+
+	const disposableTerminateBlindJs = vscode.commands.registerCommand('extension.terminateBlindJs', function () {
+		console.log('terminateBlindJs');
+		
+		disposableType.dispose()
+	});
+
+	context.subscriptions.push(disposableType);
+	context.subscriptions.push(disposableTerminateBlindJs);
+	context.subscriptions.push(disposableOpenWithBlindJs);
+}
+exports.activate = activate;
+
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
