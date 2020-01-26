fff1b9e1bbbf5a1c5b7fe5d92040e81ebe09bf3d
ShaiMesisterano <shai@mcterano.com>
2020-01-26 18:57:14 +0200

    build to lib/

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	mode: 'production',
 	entry: './src/blind.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'lib'),
 		filename: 'blind.min.js',
 		libraryTarget: 'window',
 		library: 'BlindJS'
