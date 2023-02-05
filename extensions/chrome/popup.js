1863da22e175a96fef963585be22232118d0a797
ShaiMesisterano <shai@mcterano.com>
2023-02-06 01:19:03 +0200

    report to GA

--- extensions/chrome/popup.js
+++ extensions/chrome/popup.js
@@ -3,15 +3,14 @@
     const terminateButton = document.getElementById('blindjs-terminate');
 
     activateButton.addEventListener('click', async () => {
+        const res = await chrome.runtime.sendMessage({action: "reportAnalytics"});
         const text = document.getElementById('blindjs-text').value;
         const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-        const response = await chrome.tabs.sendMessage(tab.id, {message: 'activate', text});
-        console.log(JSON.stringify(response));
+        const resp = await chrome.tabs.sendMessage(tab.id, {message: 'activate', text});
     });
 
     terminateButton.addEventListener('click',  async() => {
         const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
         const response = await chrome.tabs.sendMessage(tab.id, {message: 'terminate'});
-        console.log(JSON.stringify(response));
     });
 })();
